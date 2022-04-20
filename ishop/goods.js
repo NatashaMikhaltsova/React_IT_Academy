915698e08b66a8db681a4bb0ade69f984003bc1d
Nalallia <mnatasha11@mail.ru>
2022-04-20 21:54:59 +0300

    added ishop project

---
+++ ishop/goods.js
@@ -0,0 +1,48 @@
+const GoodsTable = React.createClass({
+    displayName: 'GoodsTable',
+
+    getDefaultProps: function () {
+        return { welcome: 'Добро пожаловать в наш интернет-магазин' };
+    },
+
+    propTypes: {
+        welcome: React.PropTypes.string,
+        tableHeader: React.PropTypes.shape({
+            id: React.PropTypes.string.isRequired,
+            title: React.PropTypes.string.isRequired,
+            price: React.PropTypes.string.isRequired,
+            url: React.PropTypes.string.isRequired,
+            count: React.PropTypes.string.isRequired,
+        }),
+        goods: React.PropTypes.arrayOf(
+            React.PropTypes.shape({
+                id: React.PropTypes.number.isRequired,
+                title: React.PropTypes.string.isRequired,
+                price: React.PropTypes.number.isRequired,
+                url: React.PropTypes.string.isRequired,
+                count: React.PropTypes.number.isRequired,
+            })
+        ),
+    },
+
+    render: function () {
+        const welcomeCode = React.DOM.div({ className: "GoodsTableWelcome" }, this.props.welcome);
+        const tableHeaderCode = React.DOM.div({ key: this.props.tableHeader.id, className: 'GoodsTableRow' },
+            React.DOM.div({ className: "GoodsTableCell GoodsTableHeaderCell" }, this.props.tableHeader.id),
+            React.DOM.div({ className: "GoodsTableCell GoodsTableHeaderCell" }, this.props.tableHeader.title),
+            React.DOM.div({ className: "GoodsTableCell GoodsTableHeaderCell" }, this.props.tableHeader.price),
+            React.DOM.div({ className: "GoodsTableCell GoodsTableHeaderCell" }, this.props.tableHeader.url),
+            React.DOM.div({ className: "GoodsTableCell GoodsTableHeaderCell" }, this.props.tableHeader.count)
+        );
+        const goodsCode = this.props.goods.map(el => {
+            return React.DOM.div({ key: el.id, className: 'GoodsTableRow' },
+                React.DOM.div({ className: "GoodsTableId GoodsTableCell" }, el.id),
+                React.DOM.div({ className: "GoodsTableTitle GoodsTableCell" }, el.title),
+                React.DOM.div({ className: "GoodsTablePrice GoodsTableCell" }, el.price),
+                React.DOM.div({ className: "GoodsTableImg GoodsTableCell" }, React.DOM.img({ src: el.url })),
+                React.DOM.div({ className: "GoodsTableCount GoodsTableCell" }, el.count)
+            )
+        });
+        return React.DOM.div(null, welcomeCode, React.DOM.div({ className: "GoodsTableFrame" }, tableHeaderCode, goodsCode));
+    },
+});

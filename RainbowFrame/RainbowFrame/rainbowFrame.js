b93259f9eac781ee982723129cf6ce4008d5b87e
Nalallia <mnatasha11@mail.ru>
2022-05-18 22:22:51 +0300

    minor improvements

--- RainbowFrame/RainbowFrame/rainbowFrame.js
+++ RainbowFrame/RainbowFrame/rainbowFrame.js
@@ -7,7 +7,6 @@ export class RainbowFrame extends React.Component {
 
     static propTypes = {
         colors: PropTypes.arrayOf(PropTypes.string).isRequired,
-        text: PropTypes.string.isRequired,
     };
 
     render() {
@@ -17,9 +16,7 @@ export class RainbowFrame extends React.Component {
                 <div style={{ border: "solid 5px " + color, padding: "10px" }}>
                     {result}
                 </div>
-            return result
-        })
-
+        });
         return result;
     }
 };

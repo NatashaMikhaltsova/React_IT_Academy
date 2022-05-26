1c8d739c40fbf29b91765cdee49d79a25804bfe2
Nalallia <mnatasha11@mail.ru>
2022-05-26 22:57:19 +0300

    added client sorting

--- mobile/src/MobileCompany.js
+++ mobile/src/MobileCompany.js
@@ -2,10 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import _ from 'lodash';
 import { clientInfoEvents, EAddedClientInfo, ESavedClientInfo, ECanceledClientInfo, EDeletedClientInfo, EEditedClientInfo } from './events';
-
 import MobileClient from './MobileClient';
-import './MobileCompany.css';
 import { MobileClientInfo } from './MobileClientInfo';
+import './MobileCompany.css';
 
 class MobileCompany extends React.PureComponent {
 
@@ -25,12 +24,13 @@ class MobileCompany extends React.PureComponent {
     state = {
         name: this.props.name,
         clients: this.props.initClients,
+        filteredClients: this.props.initClients,
         mobileClientInfoMode: null,
         selectedClient: { id: null, surname: '', name: '', patronymic: '', balance: '' },
     };
 
     lastID = this.state.clients[this.state.clients.length - 1].id;
-
+    filter= 0; //0 - все клиенты, 1 - активные клиенты, 2 - заблокированные клиенты
     headers = ['Фамилия', 'Имя', 'Отчество', 'Баланс', 'Статус', 'Управление'];
 
     componentDidMount = () => {
@@ -53,45 +53,48 @@ class MobileCompany extends React.PureComponent {
         let deletionConfirmed = window.confirm(`Вы действительно хотите ударить клиента ${cl_info.surname}?`);
         if (deletionConfirmed) {
             let deletedClientInd = this.state.clients.findIndex(cl => cl.id === cl_info.id);
-
             let newClients = this.state.clients.slice();
             newClients.splice(deletedClientInd, 1);
-            this.setState({ clients: newClients });
+            let filteredClients = this.filterClients(newClients);
+            this.setState({ clients: newClients, filteredClients: filteredClients });
         }
-    }
+    };
 
     editedClientInfo = (cl_info) => {
         !_.isEqual(cl_info, this.state.selectedClient) && this.setState({ mobileClientInfoMode: 'edit', selectedClient: { ...cl_info } });
-    }
+    };
 
     addedClientInfo = (cl_info) => {
         let newClients = this.state.clients.slice();
-        newClients = [...newClients, cl_info]
-        this.setState({ clients: newClients, mobileClientInfoMode: 'null' });
+        newClients = [...newClients, cl_info];
+        let filteredClients = this.filterClients(newClients);
+        this.setState({ clients: newClients, mobileClientInfoMode: 'null', filteredClients: filteredClients });
         this.lastID += 1;
-    }
+        this.resetMibileClientInfo();
+    };
 
     canceledClientInfo = () => {
         this.setState({ mobileClientInfoMode: 'null' });
         this.resetMibileClientInfo();
-    }
+    };
 
     savedClientInfo = (cl_info) => {
         let newClients = this.state.clients.slice();
         newClients.forEach((el, ind) => {
-            if (el.id === cl_info.id && JSON.stringify(el) !== JSON.stringify(cl_info)) {
+            if (el.id === cl_info.id && !_.isEqual(el, cl_info)) {
                 let newClient = { ...el, ...cl_info };
                 newClients[ind] = newClient
             };
         });
-        this.setState({ clients: newClients, mobileClientInfoMode: 'null' });
+        let filteredClients = this.filterClients(newClients);
+        this.setState({ clients: newClients, mobileClientInfoMode: 'null', filteredClients: filteredClients });
         this.resetMibileClientInfo();
-    }
+    };
 
     resetMibileClientInfo(id = null) {
         let emptySelectedClient = { id: id, surname: '', name: '', patronymic: '', balance: '' };
         !_.isEqual(emptySelectedClient, this.state.selectedClient) && this.setState({ selectedClient: emptySelectedClient });
-    }
+    };
 
     setMTCName = () => {
         this.setState({ name: 'MTC' });
@@ -106,11 +109,37 @@ class MobileCompany extends React.PureComponent {
         this.resetMibileClientInfo(this.lastID + 1);
     };
 
+    filterClients = (clients = this.state.clients) => {
+        let filteredClients;
+        if (this.filter === 0) filteredClients = clients;
+        else {
+            filteredClients = clients.filter(el => this.filter === 1 ? el.balance >= 0 : el.balance < 0);
+        }
+        return filteredClients;
+    };
+
+    showAllClients = () => {
+        this.filter = 0;
+        this.setState({ filteredClients: this.filterClients() });
+    };
+
+    showActiveClients = () => {
+        this.filter = 1;
+        let activeClients = this.filterClients();
+        !_.isEqual(this.state.filteredClients, activeClients) && this.setState({ filteredClients: activeClients });
+    };
+
+    showBlockedClients = () => {
+        this.filter = 2;
+        let blockedClients = this.filterClients();
+        !_.isEqual(this.state.filteredClients, blockedClients) && this.setState({ filteredClients: blockedClients });
+    };
+
     render() {
 
         console.log("MobileCompany render");
 
-        let clientsCode = this.state.clients.map(client =>
+        let clientsCode = this.state.filteredClients.map(client =>
             <MobileClient key={client.id} info={client} />
         );
 
@@ -128,9 +157,9 @@ class MobileCompany extends React.PureComponent {
                     <div className='MobileCompanyName'>Компания &laquo;{this.state.name}&raquo;</div>
                 </div>
                 <div className='MobileCompanyClientsFilter'>
-                    <input type="button" value="Все" />
-                    <input type="button" value="Активные" />
-                    <input type="button" value="Заблокированные" />
+                    <input type="button" value="Все" onClick={this.showAllClients} />
+                    <input type="button" value="Активные" onClick={this.showActiveClients} />
+                    <input type="button" value="Заблокированные" onClick={this.showBlockedClients} />
                 </div>
                 <div className='MobileCompanyTable'>
                     <div className='MobileCompanyTableHeader'>{clientHeader}</div>
--- mobile/src/events.js
+++ mobile/src/events.js
@@ -1,9 +1,9 @@
-import {EventEmitter} from 'events';
+import { EventEmitter } from 'events';
 
 const clientInfoEvents = new EventEmitter(); //все события, связанные с изменением информации о клиенте;
-const EAddedClientInfo = 'EAddedClientInfo'; //кликнута кнопка добавления нового клиена, его сэмиттирует ClientDescription и примет MobileCompany
-const ESavedClientInfo = 'ESavedClientInfo'; //кликнута кнопка сохранения информации о клиенте, его сэмиттирует ClientDescription и примет MobileCompany
-const ECanceledClientInfo = 'ECanceledClientInfo'; //кликнута кнопка отмены изменения/добавления информации о клиенте, его сэмиттирует ClientDescription и примет MobileCompany
+const EAddedClientInfo = 'EAddedClientInfo'; //кликнута кнопка добавления информации о новом клиенте, его сэмиттирует MobileClientInfo и примет MobileCompany
+const ESavedClientInfo = 'ESavedClientInfo'; //кликнута кнопка сохранения информации о клиенте, его сэмиттирует MobileClientInfo и примет MobileCompany
+const ECanceledClientInfo = 'ECanceledClientInfo'; //кликнута кнопка отмены изменения/добавления информации о клиенте, его сэмиттирует MobileClientInfo и примет MobileCompany
 const EDeletedClientInfo = 'EDeletedClientInfo'; //кликнута кнопка удаления информации о клиенте, его сэмиттирует MobileClient и примет MobileCompany
 const EEditedClientInfo = 'EEditedClientInfo'; //кликнута кнопка редактирования информации о клиенте, его сэмиттирует MobileClient и примет MobileCompany
 
